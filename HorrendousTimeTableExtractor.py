bbd3a14ef7bf251d28d6d2400440bf75f8871c10
kaawan <kaawan.jdr@gmail.com>
2024-07-25 15:07:28 +0200

    added first implementation of filters

--- HorrendousTimeTableExtractor.py
+++ HorrendousTimeTableExtractor.py
@@ -7,16 +7,19 @@ from pathlib import Path
 # TODO : add filters
 # TODO : cleanup Event object
 # TODO : cleanup code
-# TODO : add RSS compat ?
+
+# URL utilisée pour fetch les EDT de chaque filiere
+url = {
+    "INGE" : "https://aderead.univ-orleans.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=e476946ed5447b1050d7a6c48ccf4bad4592592d3b282f749c9a606b710f264f6250ba3fea2e12caebbcd166cfe88476f6893987a472171c27978e76da251b877c79900682576cf2be48a3169e635e57166c54e36382c1aa3eb0ff5cb8980cdb,1",
+    "MIAGE" : "https://aderead.univ-orleans.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=047ece9af44e28b7aebedd6848891aa04592592d3b282f749c9a606b710f264f6250ba3fea2e12caebbcd166cfe88476574604eff8c5d3443632e6f8ddd25c327c79900682576cf2be48a3169e635e57166c54e36382c1aa3eb0ff5cb8980cdb,1",
+}
 
 weekday = [
-    "Lundi",
-    "Mardi",
-    "Mercredi",
-    "Jeudi",
-    "Vendredi",
-    "Samedi",
-    "Dimanche"
+    "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"
+]
+
+month = [
+    "Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"
 ]
 
 subjects_table = {
@@ -28,7 +31,7 @@ subjects_table = {
 }
 
 class Event:
-    """Class used to manage event objects"""
+    """Classe utilisée pour gerer les objets evenements"""
     def __init__(self, start : datetime, end :datetime, subject : str, group :str, location:str, teacher:str, isINGE:bool, isMIAGE:bool) -> None:
         self.start_timestamp = start
         self.end_timestamp = end
@@ -41,9 +44,22 @@ class Event:
         self.isINGE  = isINGE
     
     def __str__(self) -> str:
-        return f"{self.summary} : {self.location}\n{self.description}\n{self.start_timestamp.ctime()} - {self.end_timestamp.ctime()}"
+        return f"{self.start_timestamp.strftime("%Hh%M")}-{self.end_timestamp.strftime("%Hh%M")} : {"MIAGE" if (self.isMIAGE) else ""}{" - " if (self.isINGE and self.isMIAGE) else ""}{"INGE" if (self.isINGE) else ""} {self.group} - {self.subject} - {self.location} - {self.teacher}"
+
+        
+def fetch_calendar(url:str, filename:str):
+    """Récupere le fichier .ics correspondant a une filiere donnée"""
+    urlretrieve(url, filename)
+
+
+def convert_timestamp(input : str) -> datetime :
+    """Permet de convertir les timestamp en ISO-8601, et les passer en UTC+2"""
+    iso_date = f"{input[0:4]}-{input[4:6]}-{input[6:11]}:{input[11:13]}:{input[13:]}"
+    return datetime.fromisoformat(iso_date).astimezone(timezone("Europe/Paris"))
+
 
 def build_event_from_data(start : datetime, end :datetime, sum : str, loc :str, desc :str) -> Event:
+    """Permet d'extraire les informations des données parsées"""
     descsplit = desc.split("\\n")
     subject = subjects_table[descsplit[3]] if descsplit[3] in subjects_table.keys() else descsplit[3]
     teacher = descsplit[-3].replace("\n", "").removeprefix(" ")
@@ -71,14 +87,13 @@ def build_event_from_data(start : datetime, end :datetime, sum : str, loc :str, 
     
     return Event(start, end, subject, group, location, teacher, isINGE, isMIAGE)
 
-def convert_timestamp(input : str) -> datetime :
-    iso_date = f"{input[0:4]}-{input[4:6]}-{input[6:11]}:{input[11:13]}:{input[13:]}"
-    return datetime.fromisoformat(iso_date).astimezone(timezone("Europe/Paris"))
 
-def parse_calendar(filename: str = "input/ADECal.ics") -> list[Event]:
+def parse_calendar(filiere:str="INGE") -> list[Event]:
+    """Extrait les données du fichier .ics et le telecharge si il est manquant"""
+    filename = f"input/{filiere}.ics"
     p = Path(filename)
     if not p.exists() :
-        fetch_calendar(filename)
+        fetch_calendar(url[filiere], filename)
 
     with open(filename, "r", encoding="utf-8") as f:
         lines = f.readlines()
@@ -108,18 +123,37 @@ def parse_calendar(filename: str = "input/ADECal.ics") -> list[Event]:
                     break
     return sorted(events, key=lambda event: event.start_timestamp)
 
-def display(events : list[Event]):
-    current_weekday = -1
+
+def filter_events(events:list[Event], before:date=None, after:date=None, filiere:str="") -> list[Event]:
+    output = []
+
+    for event in events:
+        if before != None:
+            if event.end_timestamp.date() > before :
+                continue
+        if after != None:
+            if event.start_timestamp.date() < after :
+                continue
+        if (filiere == "MIAGE" and not event.isMIAGE) or (filiere=="INGE" and not event.isINGE) :
+            continue
+        
+        output.append(event)
+    return output
+
+
+def display(events:list[Event]) -> None:
+    """affiche une liste d'évenements"""
+    current_weekday = 7
+
     for event in events:
         if event.start_timestamp.weekday() != current_weekday:
-            print(f"**{weekday[current_weekday]} {event.start_timestamp.day}:**")
             current_weekday = event.end_timestamp.weekday()
-        print( f"{event.start_timestamp.strftime("%Hh%M")}-{event.end_timestamp.strftime("%Hh%M")} : {"MIAGE" if (event.isMIAGE) else ""}{" - " if (event.isINGE and event.isMIAGE) else ""}{"INGE" if (event.isINGE) else ""} {event.group} - {event.subject} - {event.location} - {event.teacher}")
+            print(f"**{weekday[current_weekday]} {event.start_timestamp.day} {month[event.start_timestamp.month -1]}:**")
+        print(event)
 
+events = parse_calendar("INGE")
 
-def fetch_calendar(filename:str = "input/ADECal.ics"):
-    url = ("https://aderead.univ-orleans.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=e476946ed5447b1050d7a6c48ccf4bad4592592d3b282f749c9a606b710f264f6250ba3fea2e12caebbcd166cfe88476f6893987a472171c27978e76da251b877c79900682576cf2be48a3169e635e57166c54e36382c1aa3eb0ff5cb8980cdb,1")
-    urlretrieve(url, filename)
+filtered_events = filter_events(events, before=date(2024,10,1), filiere="INGE")
 
-display(parse_calendar())
+display(filtered_events)
 
